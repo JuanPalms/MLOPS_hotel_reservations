ebd0ee33ac13fb1e9a71eeeaf89226e1caae9b29
JuanPalms <palmerosjuanfrancisco@gmail.com>
2025-10-22 20:40:57 -0600

    Adds training pipeline

---
+++ utils/common_functions.py
@@ -0,0 +1,39 @@
+import os
+import pandas
+from src.logger import get_logger
+from src.custom_exception import CustomException
+import yaml
+import pandas as pd
+
+logger = get_logger(__name__)
+
+def read_yaml(file_path):
+  """Function that is used to read yaml files
+  Args:
+  file_path (str): reads a string continaing a file path
+  Returns:
+  config: Yaml structure"""
+  try:
+    if not os.path.exists(file_path):
+      raise FileNotFoundError(f"File is not in the given path")
+    
+    with open(file_path, "r") as yaml_file:
+      config = yaml.safe_load(yaml_file)
+      logger.info("Successfully read the YAML file")
+      return config
+  except Exception as e:
+    logger.error("Error while reading YAML file")
+    raise CustomException("Failed to read YAML file", e)
+  
+
+def load_data(path):
+  try:
+    logger.info("Loading data")
+    return pd.read_csv(path)
+  except Exception as e:
+    logger.error(f"Error loading the data {e}")
+    raise CustomException("Failed to load data", e)
+  
+
+
+  
